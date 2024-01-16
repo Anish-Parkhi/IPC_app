daf1f48cfd0d9a5e493c0a1abbb57be793736886
Anish-Parkhi <anishparkhi03@gmail.com>
2024-01-16 19:01:21 +0530

    added the gradle config files

--- ios/IPCInsightsTests/IPCInsightsTests.m
+++ ios/IPCInsightsTests/IPCInsightsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ipcTests : XCTestCase
+@interface IPCInsightsTests : XCTestCase
 
 @end
 
-@implementation ipcTests
+@implementation IPCInsightsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
